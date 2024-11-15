4a8611df955f6e491f6649185257dcffb9ea9ec1
himanshu-civilgeo <sharma.himanshu@highlands.in>
2024-11-15 19:01:52 +0530

    tailwind imported

---
+++ counter-number/src/tailwind.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+import Card from './components/card'
+
+function Tailwind() {
+  
+    const [cards, setCards] = useState(0)
+
+  return ( 
+    <>
+        <h4 className='bg-green-400 text-black p-4 rounded-xl mb-8'>Tailwind Test Heading</h4>
+        
+        <Card />
+          
+    </>
+  )
+}
+
+export default Tailwind
